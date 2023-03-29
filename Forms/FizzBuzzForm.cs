a42cce2355470600660ba92ac0413ad57564c887
Bocian <szyper212pl@gmail.com>
2023-03-29 14:37:08 +0200

    z zajeć nr 2. Fizz, Buzz i FizzBuzz bez centrowania alertu

---
+++ Forms/FizzBuzzForm.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace zajecia1.Forms
+{
+    
+    public class FizzBuzzForm
+    {
+        public String CheckValue (int? Number)
+        {
+            string value = "";
+            if (Number % 3 == 0)
+            {
+                value = "Fizz";
+                if (Number % 5 == 0)
+                {
+                    value += "Buzz";
+                }
+            }
+            else if (Number % 5 == 0)
+            {
+                value = "Buzz";
+            }
+            /*if (Number % 3 == 0 && Number % 5 == 0)
+            {
+                value = "FizzBuzz";
+            }*/
+            else { return "Liczba " + Number + " nie oddaje"; };
+            return value;
+        }
+        [Display(Name = "Twój szczęśliwy numerek")]
+        [Required, Range(1, 1000, ErrorMessage = "Podaj od 1 do 1000")]
+        public int? Number { get; set; }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using zajecia1.Forms;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace zajecia1.Pages
@@ -6,7 +7,13 @@ namespace zajecia1.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private int? Number;
 
+        [BindProperty]
+        public FizzBuzzForm FizzBuzz { set; get; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public string Result { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +21,19 @@ namespace zajecia1.Pages
 
         public void OnGet()
         {
-
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public IActionResult OnPost()
+        {
+            Result = FizzBuzz.CheckValue(FizzBuzz.Number);
+            /*if (ModelState.IsValid)
+            {*/
+                return Page();
+            /*}
+            return RedirectToPage("./Privacy");*/
         }
     }
 }
